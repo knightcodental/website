fd625f20acb5b918c697a7ac9c41f11735d2f698
knightcodental <knightco1972@gmail.com>
2026-07-10 13:04:32 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-console.log("Knight Co Dental Website Loaded");
-
 document.addEventListener("DOMContentLoaded", function () {
-  const button = document.querySelector("button");
 
-  if (button) {
-    button.addEventListener("click", function () {
-      alert("Welcome to Knight Co Dental!");
-    });
-  }
+    const button = document.querySelector("button");
+
+    if (button) {
+        button.addEventListener("click", function () {
+            alert("Thank you for visiting Knight Co Dental!\n\nPlease contact us for our latest Dental Instruments.");
+        });
+    }
+
+    console.log("Knight Co Dental Website Loaded Successfully");
+
 });
